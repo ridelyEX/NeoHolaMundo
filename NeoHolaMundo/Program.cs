9d26cd543db01e38975e491d4567d49fc9e8ea1d
ridelyEX <arturovalruiz@gmail.com>
2026-01-08 13:29:14 -0600

    NeoHolaMundo terminado

--- NeoHolaMundo/Program.cs
+++ NeoHolaMundo/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string opc;
+        int personas = 0;
+
+        do
+        {
+            string? nombre;
+            int edad;
+
+            // Lectura de nombre
+            do
+            {
+                Console.WriteLine("Ingrese un nombre");
+                nombre = Console.ReadLine();
+            } while (string.IsNullOrEmpty(nombre));
+
+            // Lectura de edad
+            Console.WriteLine("Ingresa edad");
+            while (!int.TryParse(Console.ReadLine(), out edad))
+            {
+                Console.WriteLine("Ingresa un número válido");
+            }
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Eres mayor de edad, puedes pasar");
+                Console.WriteLine("Hola, " + nombre);
+                personas++;
+            }
+            else
+                Console.WriteLine("Eres menor de edad, acceso restringido");
+            
+            Console.WriteLine($"personas autorizadas ingresadas: {personas}");
+
+
+            // Lectura de opción
+            do
+            {
+                Console.WriteLine("Desea ingresar a otra persona? si para continuar, no o salir para cerrar la ejecución");
+                opc = Console.ReadLine().ToLower().Trim();
+            } while (string.IsNullOrEmpty(opc));
+            
+
+        } while ( opc == "si" );
+
+        Console.Write("Ejecución terminada. Hasta pronto");
+    }
+}
